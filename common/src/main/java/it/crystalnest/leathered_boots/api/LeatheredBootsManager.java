92ab872ffbc9aea1e945579ebd01bd51c2dd2daa
Crystal Spider <riccardo.chimisso@gmail.com>
2024-12-20 22:35:06 +0100

    Fix some Javadoc.

--- common/src/main/java/it/crystalnest/leathered_boots/api/LeatheredBootsManager.java
+++ common/src/main/java/it/crystalnest/leathered_boots/api/LeatheredBootsManager.java
@@ -109,6 +109,8 @@ public final class LeatheredBootsManager {
 
   /**
    * Register for {@link LeatheredBootsItem}s.
+   *
+   * @param modId mod ID.
    */
   public record BootsRegister(String modId) {
     /**
